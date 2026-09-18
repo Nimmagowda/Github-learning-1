245be49fd8ff651f1e87cc5b64338b1da96a6746
PRABHUGOUDA BIRADAR <prabhugoudabiradar49@gmail.com>
2026-09-18 09:39:09 +0000

    ==adding new feature 3

--- shop.py
+++ shop.py
@@ -27,13 +27,15 @@ class Shop:#creating class with name of shop
 
         }
         print(products)
+        print("========================================================================")
         return products
 
     def add_cart(self):
         self.item_details()
-        for i in range(1,5):
-            select =input("select your items : ")
-        
+        while True:
+            select =input("select your items(or type done) : ")
+            if select == "done":
+                 break
             for category,products in self.item_details().items():
                 if select in products:
                     self.cart[select] = products[select]
@@ -41,12 +43,14 @@ class Shop:#creating class with name of shop
                     #print(self.cart)
                     break    
             else:
-                print("product not found : ")         
+                print("product not found : ")   
+        print("========================================================================")      
         return self.cart
     def total_items(self):
         print(self.cart)
         self.amount = sum(self.cart.values())
         print("Total amount",self.amount)
+        print("========================================================================")
         print("1 : Phone pay")
         print("2 : Google pay")
         print("3 : cash")
@@ -57,6 +61,8 @@ class Shop:#creating class with name of shop
                     print("Go Through with Google pay")
         elif choice == 3:
                     print("Go Through with Cash")
+        else:
+            print(f"{choice}this type payment not allowed")
 
 
         
@@ -74,9 +80,12 @@ while True:
     elif choice == 2:
         print(c1.add_cart())
     elif choice == 3:
+        print("========================================================================")
         print(c1.total_items())
         
     elif choice == 5:
             print("exiting")
             print("Thanks for Visiting")
             break
+    else:
+         print("invalid choice")
